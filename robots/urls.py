19c8cb76705885a65dc4c6ca124ca6bd931f782b
6UEINNTY00E1 <89670253660@mail.ru>
2023-09-26 11:57:25 +0300

    Add week report

--- robots/urls.py
+++ robots/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import add_robot
+from .views import add_robot, get_report
 
 app_name = 'robots'
 
 urlpatterns = [
     path('add_robot/', add_robot, name='add_robot'),
+    path('weekly_report/', get_report, name='report'),
 ]
